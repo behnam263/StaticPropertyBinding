a53671751ab36404b30f745006a0625c72a59865
Behi <Behnam263@yahoo.com>
2016-04-09 00:07:27 +0430

    Static bind property .net 4.5 not efficient but available

---
+++ StaticBindingTest/model.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticBindingTest
+{
+
+
+    public static class model
+    {
+        public static event EventHandler modelTitleChanged;
+
+        private static string _modelTitle;
+        public static string modelTitle
+        {
+            get { return _modelTitle; }
+            set
+            {
+                if (value != _modelTitle)
+                {
+                    _modelTitle = value;
+
+                    if (modelTitleChanged != null)
+                        modelTitleChanged(null, EventArgs.Empty);
+                }
+            }
+        }
+    }
+}
